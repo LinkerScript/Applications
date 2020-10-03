4939c7d4f0711bc4198f03f5e85431b71d2a708b
Circumcision <71528373+Circumcision@users.noreply.github.com>
2020-10-02 22:25:19 -0400

    Create Client.js

---
+++ client/Client.js
@@ -0,0 +1,28 @@
+const { Client, Collection } = require('discord.js');
+const { join } = require('path');
+const { readdirSync } = require('fs');
+const { token } = 'you thought'
+
+class ApplicationClient extends Client {
+    constructor(){
+        super();
+        this.commands = new Collection()
+    };
+    start(token, cmd){
+        this.prefix = '/';
+        this.login(token);
+        readdirSync(join(process.cwd(), '..', cmd)).map((data) => {
+            const file = require(join(process.cwd(), `..`, cmd, data));
+            this.commands.set(file.name, file)
+        });
+        this.on('ready', () => console.log(`Logged in!`))
+        this.on('message', async(message) => {
+            if(message.author.bot || !message.guild || !message.content.toLowerCase().startsWith(this.prefix)) return;
+            const [ cmd, ...args ] = message.content.slice(this.prefix.length).trim().split(/ +/g);
+            const command = this.commands.get(cmd.toLowerCase());
+            if(!command) return;
+            command.run(this, message, args).catch(console.error);
+        })
+    }
+};
+module.exports = ApplicationClient;
